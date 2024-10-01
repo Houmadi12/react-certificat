de973152901043cde98c1bf0ce5d9de1a84a01ff
Ambdouroihmane Soilihi <ambdousoilihiit@gmail.com>
2024-10-01 19:55:56 +0000

    étape 3

--- partie1/courseinfo/src/App.js
+++ partie1/courseinfo/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 const Header = (props) => {
+  console.log("Header props",props)
   return (
     <div>
       <h1>{props.course}</h1>
@@ -30,12 +31,43 @@ const Total = (props) => {
   )
 }
 
+// const App = () => {
+//   return(
+//     <div>
+//       <Header course = {'Half Stack application development'} />
+//       <Content />
+//       <Total exercice = {10+7+14} />
+//     </div>
+//   )
+// }
+
 const App = () => {
-  return(
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
     <div>
-      <Header course = {'Half Stack application development'} />
-      <Content />
-      <Total exercice = {10+7+14} />
+      <h1>{course}</h1>
+      <p>
+        {part1.name} {part1.exercises}
+      </p>
+      <p>
+        {part2.name} {part2.exercises}
+      </p>
+      <p>
+        {part3.name} {part3.exercises}
+      </p>
     </div>
   )
 }
